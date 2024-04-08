43f13e7346a3576d6f494861823ebc2893d6cc77
He Jinxin <jxhe@mail.ustc.edu.cn>
2024-04-08 22:45:22 +0800

    新增加法和减法

--- 9cc.c
+++ 9cc.c
@@ -6,10 +6,30 @@ int main(int argc, char ** argv){
         fprintf(stderr, "引用数量错误\n");
         return -1;
     }
+	char *p = argv[1];
+
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("    mov rax, %d\n", atoi(argv[1]));
+    printf("	mov rax, %ld\n", strtol(p, &p, 10));
+	
+
+	while(*p) {
+		if(*p == '+'){
+			p++;
+			printf("	add rax, %ld\n", strtol(p, &p, 10));
+			continue;
+		}
+		
+		if(*p == '-'){
+			p++;
+			printf("	sub rax, %ld\n", strtol(p, &p, 10));
+			continue;
+		}
+		
+		fprintf(stderr, "预料之外的字符: '%c'\n", *p);
+		return 1;
+	}
     printf("    ret\n");
     
     return 0;
